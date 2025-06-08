0cdfb59cf76538caab620d416d6d206be574d440
Thais Carolina Braga de Magalhães <thaisbraga969@gmail.com>
2025-06-07 21:45:45 -0400

    adicionado biblioteca GSON + criado saveManager

---
+++ src/main/java/persistencia/SaveManager.java
@@ -0,0 +1,10 @@
+package persistencia;
+
+/*
+    Explicando o que vou fazer aqui!
+    Vou salvar os atributos em um arquivo JSON porque, pelo que eu analisei
+    sendo um arquivo JSON vai facilitar a questao de delimitar informacao e afins
+ */
+
+public class SaveManager {
+}
