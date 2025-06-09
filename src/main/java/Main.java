5194bcea8e8a94823b16c291f42017e730a07387
Joao Pedro Telles Paes <jptpa.snf24@uea.edu.br>
2025-06-09 19:23:21 -0400

    ultimo mesmo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,4 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-import negocios.*;
-import negocios.tipos_item.*;
-import negocios.tipos_item.ItensAtk.*;
-import negocios.tipos_item.ItensConsumiveis.CuraGrande;
-import negocios.tipos_item.ItensConsumiveis.CuraMedia;
-import negocios.tipos_item.ItensConsumiveis.CuraPequena;
-import negocios.tipos_item.ItensConsumiveis.Garrafa;
-import telas.Inventario_EscolherNovoItem;
-import telas.ScreenManager;
-import telas.Sprite;
-
-import java.util.ArrayList;
+import negocios.GameManager;
 
 public class Main {
   public static void main(String[] args) {
